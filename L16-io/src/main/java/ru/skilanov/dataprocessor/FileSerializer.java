19cea99b80e440f5b5707125c5d25a6baa401238
Kilanov <sam.kilanoff@gmail.com>
2022-10-06 23:05:48 +0500

    L16-io add hw with json serialization

---
+++ L16-io/src/main/java/ru/skilanov/dataprocessor/FileSerializer.java
@@ -0,0 +1,25 @@
+package ru.skilanov.dataprocessor;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class FileSerializer implements Serializer {
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void serialize(Map<String, Double> data) {
+        try (var writer = new BufferedWriter(new FileWriter(this.fileName))) {
+            writer.write(new Gson().toJson(data));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ L16-io/src/main/java/ru/skilanov/dataprocessor/ProcessorAggregator.java
@@ -0,0 +1,23 @@
+package ru.skilanov.dataprocessor;
+
+import ru.skilanov.model.Measurement;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ProcessorAggregator implements Processor {
+
+    @Override
+    public Map<String, Double> process(List<Measurement> data) {
+        return data.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Measurement::getName,
+                                LinkedHashMap::new,
+                                Collectors.summingDouble(Measurement::getValue)
+                        )
+                );
+    }
+}
